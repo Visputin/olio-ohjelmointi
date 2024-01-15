e9986150d0a8a4ed4d3d3d75cbe08572aff82033
Saska <lasaroffs@gmail.com>
2024-01-15 09:35:35 +0200

    classExample1 aloitettu

---
+++ classExample1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// esimerkki student-luokasta, jolla
+// private tyyppiset jäsenmuuttujat fname, lname
+// ja niille public tyyppiset Getter ja Setter metodit
+// sekä public tyyppinen metodi StudentInfo, joka tulostaa tiedot
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
