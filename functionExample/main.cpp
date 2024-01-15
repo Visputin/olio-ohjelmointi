6ac30d06e73c984f11112ce4bbf87b3ecd6ff091
Saska <lasaroffs@gmail.com>
2024-01-15 09:02:51 +0200

    functionExample valmis

--- functionExample/main.cpp
+++ functionExample/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 
+/* kirjoita funktio, joka ottaa vastaan kaksi kokonaislukua.
+ * Funktio kertoo molemmat luvut kahdella ja tulostaa summan.
+ * Tulosta myös alkuperäiset luvut. */
+
 using namespace std;
+void doubleNumbers1 (int x, int y);
+void doubleNumbers2(int *x, int *y);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int x = 5, y = 9;
+    cout << "aluksi mainissa x = "  << x << endl;
+    cout << "aluksi mainissa y = "  << y << endl;
+
+    doubleNumbers1(x,y);
+    cout << "lopuksi mainissa x = "  << x << endl;
+    cout << "lopuksi mainissa y = "  << y << endl;
+
+
+    doubleNumbers2(&x, &y);
+    cout << "lopuksi mainissa x = "  << x << endl;
+    cout << "lopuksi mainissa y = "  << y << endl;
     return 0;
+}
+
+void doubleNumbers1 (int x, int y){
+    x = x * 2;
+    y = y * 2;
+    cout << "funktiossa (1) x = " << x << endl;
+    cout << "funktiossa (1) y = " << y << endl;
+}
+
+void doubleNumbers2(int *x, int *y){
+    *x = *x * 2;
+    *y = *y * 2;
+    cout << "funktiossa (2) x = " << *x << endl;
+    cout << "funktiossa (2) y = " << *y << endl;
 }
