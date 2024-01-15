65317bf2b10ca58ffd4fdf6084538b9054fdb459
Saska <lasaroffs@gmail.com>
2024-01-15 09:55:53 +0200

    classExample1 aloitettu

--- classExample1/main.cpp
+++ classExample1/main.cpp
@@ -1,3 +1,5 @@
+#include "student.h"
+
 #include <iostream>
 
 using namespace std;
@@ -5,10 +7,14 @@ using namespace std;
 // esimerkki student-luokasta, jolla
 // private tyyppiset jäsenmuuttujat fname, lname
 // ja niille public tyyppiset Getter ja Setter metodit
-// sekä public tyyppinen metodi StudentInfo, joka tulostaa tiedot
+// sekä public tyyppinen metodi studentInfo, joka tulostaa tiedot
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // luodaan Student-luokasta olio pinoon
+    Student objectStudent1;
+    objectStudent1.setFname("Teppo");
+    objectStudent1.setLname("Testi");
+    objectStudent1.studentInfo();
     return 0;
 }
---
+++ classExample1/student.cpp
@@ -0,0 +1,29 @@
+#include "student.h"
+
+
+string Student::getFname() const
+{
+    return fname;
+}
+
+void Student::setFname(const string &newFname)
+{
+    fname = newFname;
+}
+
+string Student::getLname() const
+{
+    return lname;
+}
+
+void Student::setLname(const string &newLname)
+{
+    lname = newLname;
+}
+
+void Student::studentInfo()
+{
+    cout << "Etunimeni on " << fname << "Sukunimeni on " << lname << endl;
+}
+
+Student::Student() {}
